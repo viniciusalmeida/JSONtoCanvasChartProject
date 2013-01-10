1e86cf43ca4c4cb9d32e0ec45cdf0e77dff6121b
Vinícius Almeida da Silva <hey@viniciusalmeida.com>
2013-01-10 01:16:23 -0200

    cores nos gráficos, abstração de métodos
    - Adição de cores nas propriedades dos gráficos;
    - Abstração do desenho de retangulos;
    - Abstração do desenho de cada tipo de gráfico;
    - Implementação do desenho de gráficos básicos (barras);
    - Alteração de comentários (de inglês para português);
    - Organização dos métodos públicos;

--- JSONtoCanvasChartProject.js
+++ JSONtoCanvasChartProject.js
@@ -1,85 +1,152 @@
-/* Simple tool to convert JSON expressions in Charts built with HTML5 canvas */
+/* Ferramenta simples para a conversão de expressõe JSON para gráficos construídos 
+com o elemento canvas do HTML5 */
 
+var JSONtoCanvasChart = function (larguraCanvas, alturaCanvas) {
 
-var JSONtoCanvasChart = function (widthCanvas, heightCanvas) {
-
-
-    /* Definition of the properties */
+    /* Definição das propriedades e dos seus valores iniciais */
     var canvas,
         context,
         dataCollection,
-        widthChart = widthCanvas,
-        heightChart = heightCanvas,
-        widthGrid,
-        heightGrid;
-
+        larguraGrafico = larguraCanvas,
+        alturaGrafico = alturaCanvas,
+        tamanhoMargem,
+        propriedadesGrafico = {'backgroundColor': '#FFF',
+                               'borderColor': '#000'},
+        larguraGrid,
+        alturaGrid,
+        propriedadesGrid = {'backgroundColor': '#FFF',
+                            'borderColor': '#CCC',
+                            'tipoGrafico': 'barras',
+                            'cores': ['red', 'green', 'blue', 'yellow']};
 
     function load() {
-        /* Pseudo-contructor method.
-        defines the size of the chart/canvas, variable context and 
-        background color on our chart. */
+        /* Método pseudo-construtor
+        Define o tamanho do gráfico/canvas e o contexto do canvas.
+        Aqui já começamos a desenhar nosso gráfico.*/
         canvas = document.createElement('canvas');
-        canvas.setAttribute('width', widthChart);
-        canvas.setAttribute('height', heightChart);
+        canvas.setAttribute('width', larguraGrafico);
+        canvas.setAttribute('height', alturaGrafico);
         context = canvas.getContext('2d');
-        context.fillStyle = '#000';
-        context.fillRect(0, 0, widthChart, heightChart);
+        desenhaRect(propriedadesGrafico.backgroundColor, 0, 0, 
+                    larguraGrafico, alturaGrafico, propriedadesGrafico.borderColor);
     }
 
-
-    /* Pseudo-contructor method call */
+    /* Chamada ao pseudo-construtor */
     load();
 
-
-    function setGrid() {
-        /* Method that defines the properties of internal grid on our chart. */
-        var marginSize;
-        if (widthChart < heightChart) {
-            marginSize = Math.round(widthChart - (widthChart * 0.95));
-        } else {
-            marginSize = Math.round(heightChart - (heightChart * 0.95));
+    function desenhaRect (corPreenchimento, posicaoX, posicaoY, 
+                          largura, altura, corContorno) {
+        /* Método que abstrai o desenho de retangulos no canvas.
+        A cor do contorno é opcional. Se ela não for setada o retangulo é desenhado sem bordas. */
+        context.fillStyle = corPreenchimento;
+        context.fillRect(posicaoX, posicaoY, largura, altura);
+        if (corContorno !== undefined) {
+            context.strokeStyle = corContorno;
+            context.lineWidth = 1;
+            context.strokeRect(posicaoX, posicaoY, largura, altura)
         }
-        console.log(marginSize);
-        widthGrid = widthChart - Math.round(marginSize * 2);
-        heightGrid = heightChart - Math.round(marginSize * 2);
-        context.fillStyle = '#999';
-        context.fillRect(marginSize, marginSize, widthGrid, heightGrid);
     }
 
+    function setGrid() {
+        /* Método que define as proporções da margem interna entre a borda do gráfico e
+        e o grid. */
+        if (larguraGrafico < alturaGrafico) {
+            tamanhoMargem = Math.round(larguraGrafico - (larguraGrafico * 0.9));
+        } else {
+            tamanhoMargem = Math.round(alturaGrafico - (alturaGrafico * 0.9));
+        }
+
+        larguraGrid = larguraGrafico - Math.round(tamanhoMargem * 2);
+        alturaGrid = alturaGrafico - Math.round(tamanhoMargem * 2);
+        desenhaRect(propriedadesGrid.backgroundColor, tamanhoMargem, tamanhoMargem, 
+                    larguraGrid, alturaGrid, propriedadesGrid.borderColor);
+    }
+
+    function invocaGrafico() {
+        /* Método que valida qual o tipo de gráfico que está setado e invoca
+        o método responsável pela renderização do mesmo */
+        switch (propriedadesGrid.tipoGrafico) {
+
+            case 'barras':
+                /*  */
+                // @TODO analizar a dataCollection pra ver se está no padrão do tipo de gráfico
+                desenhaBarrasSimples();
+                break;
+        }
+    }
+
+    function desenhaBarrasSimples () {
+        /* Método responsável pelo desenho do gráfico de barras simples
+        sem valores negativos (por enquento). 
+        Ele basicamente percorre os dados a serem incluídos no gráfico e efetua os
+        cálculos nescessários para desenhar o gráfico nas proporções adequadas.
+        - A largura das barras está sendo dividida por 2 para possibilitar um 
+        distanciamento entre as barras do mesmo tamanho da largura das mesmas.
+        - O iterador utilizado no for que faz o desenho do gráfico em sí inicia
+        em 0.25 pois é um valor inicial que, nos calculos de posicionamento dos
+        elementos, posiciona a primeira barra na distância exata da margem esquerda. */
+        var numeroRotulos = dataCollection.length,
+            larguraBarra = Math.round((larguraGrid / numeroRotulos) / 2),
+            alturaBarra,
+            distanciaXbarra,
+            distanciaYbarra,
+            distanciaXvalorBarra,
+            distanciaYvalorBarra,
+            maiorValorDaCollection = 0,
+            iteradorFor = 0.25,
+            corBarra;
+        /* Percorre os valores da collection de dados buscando o maior para utilizar
+        como referência do tamanho das barras do gráfico. */
+        for (var i = 0; i < dataCollection.length; i++) {
+            if (dataCollection[i].valor > maiorValorDaCollection) {
+                maiorValorDaCollection = dataCollection[i].valor;
+            }
+        }
+        /* Percorre os dados da collection para desenhar efetivamente o gráfico. */        
+        for (var i = 0; i < dataCollection.length; i++) {
+            alturaBarra = ((dataCollection[i].valor / maiorValorDaCollection) * alturaGrid) - 2;
+            distanciaXbarra = tamanhoMargem + (larguraBarra * iteradorFor) * 2;
+            distanciaYbarra = (tamanhoMargem - 1) + (alturaGrid - alturaBarra);
+            distanciaXvalorBarra = distanciaXbarra + (larguraBarra / 2);
+            distanciaYvalorBarra = distanciaYbarra + 15;
+            corBarra = propriedadesGrid.cores.shift();
+            propriedadesGrid.cores.push(corBarra);
+            iteradorFor = iteradorFor + 1;
+            desenhaRect(corBarra, distanciaXbarra, distanciaYbarra, 
+                        larguraBarra, alturaBarra);
+            context.fillStyle = '#000';
+            context.textAlign = 'center';
+            context.fillText(dataCollection[i].valor, distanciaXvalorBarra, distanciaYvalorBarra);
+        }
+    }
 
     return {
 
-
-        Chart: {
-
-
-            show: function (idContainer) {
-                /*  */
-                document.getElementById(idContainer).appendChild(canvas);
-                setGrid();
-            }
-
-
+        show: function (idContainer) {
+            /* Método que desencadeia todo o processamento da renderização dos gráficos.
+            Recebe no parâmetro o identificador do elemento que receberá o gráfico. */
+            document.getElementById(idContainer).appendChild(canvas);
+            setGrid();
+            invocaGrafico();
         },
 
-
-        Legend: {
-
-
-            show: function () {
-                /*  */
-                return;
-            }
-
-
+        setDataCollection: function (collection) {
+            /* Método setter da collection de dados que serão usados nos gráficos.
+            Ao ser invocado já converte a notação JSON. */
+            dataCollection = JSON.parse(collection);
         },
 
+        setPropriedadesGrid: function (newPropertiesObject) {
+            /* Método setter das propriedades básicas.
+            Ele trabalha de uma forma bem inteligente validando a existência das propriedades */
+            var property,
+                existingProperty;
 
-        setColecaoDados: function (jsonDataCollection) {
-            /*  */
-            return;
+            for (property in newPropertiesObject) {
+                existingProperty = propriedadesGrid.hasOwnProperty(property);
+                if (existingProperty) {propriedadesGrid[property] = newPropertiesObject[property]; }
+            }
         }
-        
 
     };
 
