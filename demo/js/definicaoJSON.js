28702ff560cea69b1aa71446c413a2ecff80c366
Vinícius Almeida da Silva <hey@viniciusalmeida.com>
2013-01-10 01:16:49 -0200

    adição de página de demonstração

---
+++ demo/js/definicaoJSON.js
@@ -0,0 +1,10 @@
+var bilheteria = [];
+bilheteria.push({'rotulo': 'Pulp Fiction',
+                 'valor': 107});
+bilheteria.push({'rotulo': 'Karatê Kid',
+                 'valor': 268});
+bilheteria.push({'rotulo': 'outro',
+                 'valor': 190});
+bilheteria.push({'rotulo': 'outro',
+                 'valor': 210});
+bilheteria = JSON.stringify(bilheteria);
